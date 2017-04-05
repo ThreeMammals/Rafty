94f861d3cfbb22f3a07069f647b805d800b2f205
Tom Gardham-Pallister <tom.pallister@fourth.com>
2017-04-05 06:51:36 +0100

    removed unused usings

--- src/Rafty/Raft/IServersInCluster.cs
+++ src/Rafty/Raft/IServersInCluster.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using Rafty.Infrastructure;
 using Rafty.ServiceDiscovery;
 
 namespace Rafty.Raft
