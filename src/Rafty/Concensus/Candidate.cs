002679e1e283d83666d07971cb1a879cdfb605f6
Tom Gardham-Pallister <tom.pallister@fourth.com>
2017-07-19 07:53:59 +0100

    tests for all servers...needs refactoring haha

--- src/Rafty/Concensus/Candidate.cs
+++ src/Rafty/Concensus/Candidate.cs
@@ -55,28 +55,50 @@ namespace Rafty.Concensus
 
         public IState Handle(AppendEntries appendEntries)
         {
+            //todo consolidate with request vote
             if(appendEntries.Term > CurrentState.CurrentTerm)
             {
-                var newState = new CurrentState(CurrentState.Id, CurrentState.Peers, appendEntries.Term, CurrentState.VotedFor, CurrentState.Timeout, CurrentState.Log);
-                return new Follower(newState, _sendToSelf);
+                var nextState = new CurrentState(CurrentState.Id, CurrentState.Peers, appendEntries.Term, CurrentState.VotedFor, CurrentState.Timeout, CurrentState.Log);
+                return new Follower(nextState, _sendToSelf);
             }
-
+            
             return this;
         }
 
         public IState Handle(RequestVote requestVote)
         {
-            throw new NotImplementedException();
+            //todo - consolidate with AppendEntries
+            if(requestVote.Term > CurrentState.CurrentTerm)
+            {
+                var nextState = new CurrentState(CurrentState.Id, CurrentState.Peers, requestVote.Term, CurrentState.VotedFor, CurrentState.Timeout, CurrentState.Log);
+                return new Follower(nextState, _sendToSelf);
+            }
+
+            return this;
         }
 
         public IState Handle(AppendEntriesResponse appendEntries)
         {
-            throw new NotImplementedException();
+             //todo - consolidate with AppendEntries and RequestVOte
+            if(appendEntries.Term > CurrentState.CurrentTerm)
+            {
+                var nextState = new CurrentState(CurrentState.Id, CurrentState.Peers, appendEntries.Term, CurrentState.VotedFor, CurrentState.Timeout, CurrentState.Log);
+                return new Follower(nextState, _sendToSelf);
+            }
+
+            return this;
         }
 
         public IState Handle(RequestVoteResponse requestVoteResponse)
         {
-            throw new NotImplementedException();
+             //todo - consolidate with AppendEntries and RequestVOte wtc
+            if(requestVoteResponse.Term > CurrentState.CurrentTerm)
+            {
+                var nextState = new CurrentState(CurrentState.Id, CurrentState.Peers, requestVoteResponse.Term, CurrentState.VotedFor, CurrentState.Timeout, CurrentState.Log);
+                return new Follower(nextState, _sendToSelf);
+            }
+
+            return this;
         }
     }
 }
--- test/Rafty.UnitTests/AllServersTests.cs
+++ test/Rafty.UnitTests/AllServersTests.cs
@@ -15,6 +15,8 @@ namespace Rafty.UnitTests
 log[lastApplied] to state machine (§5.3)
 • If RPC request or response contains term T > currentTerm:
 set currentTerm = T, convert to follower (§5.1)*/
+
+        //follower
         [Theory]
         [InlineData(0, 1, 1)]
         [InlineData(2, 1, 2)]
@@ -54,7 +56,7 @@ set currentTerm = T, convert to follower (§5.1)*/
             state.CurrentState.CurrentTerm.ShouldBe(expectedTerm);
         }
 
-          [Theory]
+        [Theory]
         [InlineData(0, 1, 1)]
         [InlineData(2, 1, 2)]
         public void ShouldSetTermAsRpcTermAndStayFollowerWhenReceivesRequestVoteResponse(int currentTerm, int rpcTerm, int expectedTerm)
@@ -64,6 +66,59 @@ set currentTerm = T, convert to follower (§5.1)*/
             var follower = new Follower(currentState, sendToSelf);
             var state = follower.Handle(new RequestVoteResponseBuilder().WithTerm(rpcTerm).Build());
             state.ShouldBeOfType<Follower>();
+            state.CurrentState.CurrentTerm.ShouldBe(expectedTerm);
+        }
+
+        //candidate
+        [Theory]
+        [InlineData(0, 2, 2, typeof(Follower))]
+        [InlineData(2, 1, 3, typeof(Candidate))]
+        public void ShouldSetTermAsRpcTermAndBecomeStateWhenReceivesAppendEntries(int currentTerm, int rpcTerm, int expectedTerm, Type expectedType)
+        {
+            var currentState = new CurrentState(Guid.NewGuid(), new List<IPeer>(), currentTerm, default(Guid), TimeSpan.FromSeconds(0), new InMemoryLog());
+            var sendToSelf = new TestingSendToSelf();
+            var candidate = new Candidate(currentState, sendToSelf);
+            var state = candidate.Handle(new AppendEntriesBuilder().WithTerm(rpcTerm).Build());
+            state.ShouldBeOfType(expectedType);
+            state.CurrentState.CurrentTerm.ShouldBe(expectedTerm);
+        }
+
+        [Theory]
+        [InlineData(0, 2, 2, typeof(Follower))]
+        [InlineData(2, 1, 3, typeof(Candidate))]
+        public void ShouldSetTermAsRpcTermAndBecomeStateWhenReceivesRequestVote(int currentTerm, int rpcTerm, int expectedTerm, Type expectedType)
+        {
+            var currentState = new CurrentState(Guid.NewGuid(), new List<IPeer>(), currentTerm, default(Guid), TimeSpan.FromSeconds(0), new InMemoryLog());
+            var sendToSelf = new TestingSendToSelf();
+            var candidate = new Candidate(currentState, sendToSelf);
+            var state = candidate.Handle(new RequestVoteBuilder().WithTerm(rpcTerm).Build());
+            state.ShouldBeOfType(expectedType);
+            state.CurrentState.CurrentTerm.ShouldBe(expectedTerm);
+        }
+
+        [Theory]
+        [InlineData(0, 2, 2, typeof(Follower))]
+        [InlineData(2, 1, 3, typeof(Candidate))]
+        public void ShouldSetTermAsRpcTermAndBecomeStateWhenReceivesAppendEntriesResponse(int currentTerm, int rpcTerm, int expectedTerm, Type expectedType)
+        {
+            var currentState = new CurrentState(Guid.NewGuid(), new List<IPeer>(), currentTerm, default(Guid), TimeSpan.FromSeconds(0), new InMemoryLog());
+            var sendToSelf = new TestingSendToSelf();
+            var candidate = new Candidate(currentState, sendToSelf);
+            var state = candidate.Handle(new AppendEntriesResponseBuilder().WithTerm(rpcTerm).Build());
+            state.ShouldBeOfType(expectedType);
+            state.CurrentState.CurrentTerm.ShouldBe(expectedTerm);
+        }
+
+        [Theory]
+        [InlineData(0, 2, 2, typeof(Follower))]
+        [InlineData(2, 1, 3, typeof(Candidate))]
+        public void ShouldSetTermAsRpcTermAndStateWhenReceivesRequestVoteResponse(int currentTerm, int rpcTerm, int expectedTerm, Type expectedType)
+        {
+            var currentState = new CurrentState(Guid.NewGuid(), new List<IPeer>(), currentTerm, default(Guid), TimeSpan.FromSeconds(0), new InMemoryLog());
+            var sendToSelf = new TestingSendToSelf();
+            var candidate = new Candidate(currentState, sendToSelf);
+            var state = candidate.Handle(new RequestVoteResponseBuilder().WithTerm(rpcTerm).Build());
+            state.ShouldBeOfType(expectedType);
             state.CurrentState.CurrentTerm.ShouldBe(expectedTerm);
         }
     }
